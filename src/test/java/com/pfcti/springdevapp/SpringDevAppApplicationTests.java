4ac3851778f8e650b1e89d0c42702e9d81106afa
hannia.serrano <hannia.sj@gmail.com>
2023-04-18 18:11:24 -0600

    Commit #1-Proyecto

---
+++ src/test/java/com/pfcti/springdevapp/SpringDevAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfcti.springdevapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
