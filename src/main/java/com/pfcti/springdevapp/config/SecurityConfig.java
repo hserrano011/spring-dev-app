0566ad4a23fac1c45f1b8985a97af0446aa29342
hannia.serrano <hannia.sj@gmail.com>
2023-05-02 18:31:17 -0600

    Commit#14 - JavaTraining: AUTH 2

--- src/main/java/com/pfcti/springdevapp/config/SecurityConfig.java
+++ src/main/java/com/pfcti/springdevapp/config/SecurityConfig.java
@@ -5,15 +5,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
+  /*  @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(); manager.createUser(User.withUsername("user")
                 .password("{noop}userPass") .roles("USER")
@@ -21,16 +18,21 @@ public class SecurityConfig {
         manager.createUser(User.withUsername("admin") .password("{noop}adminPass")
                 .roles("USER", "ADMIN")
                 .build());
-        return manager; }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf() .disable()
-                .authorizeRequests()
-                .requestMatchers(HttpMethod.DELETE)
-                .hasRole("ADMIN") .requestMatchers("/v1/api/cliente/**","/v1/api/employee/**") .hasAnyRole("USER", "ADMIN") .requestMatchers("v1/api/login/**")
-                .anonymous() .anyRequest() .authenticated() .and() .httpBasic();
-        return http.build(); }
+        return manager; }*/
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+      http.csrf().disable().authorizeRequests().requestMatchers(HttpMethod.DELETE).
+              hasRole("ADMIN").requestMatchers("/v1/api/cliente/**", "/v1/api/employee/**").
+              hasAnyRole("USER", "ADMIN").
+              requestMatchers("v1/api/login/**").
+              anonymous().anyRequest().
+              authenticated().
+              and().
+              oauth2ResourceServer().jwt()
+              .jwtAuthenticationConverter(new CustomJwtAuthenticationConverter());
+      http.oauth2Login();
+      return http.build();
+  }
 
 
 }
